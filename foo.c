36fc25f8810c6bdb91717b77c954e985cbc620f1
Davide Taraschi <127745750+Davide161@users.noreply.github.com>
2023-03-13 16:49:22 +0100

    Add files via upload

---
+++ foo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("inserisci un numero: ");
+    scanf("%d", &x);
+    printf("inserisci un secondo numero: ");
+    scanf("%d", &y);
+    printf("numeri inseriti: %d, %d\n", x, y);
+}
