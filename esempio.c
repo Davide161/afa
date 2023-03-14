bfb0d4fbf19d900b852b8357bc2e9170da709062
Davide Taraschi <127745750+Davide161@users.noreply.github.com>
2023-03-14 10:22:31 +0100

    Add files via upload

--- esempio.c
+++ esempio.c
@@ -1,3 +1,13 @@
 #include  <stdio.h>
 
-int main() {}
+int main() {
+    int age;
+    char sesso;
+    printf("Sei maschio (M), femmina (F) o altro (N): ");
+    scanf("%c", &sesso);
+    printf("Hai inserito %c\n", sesso);
+    printf("Inserisci la tua età: ");
+    scanf("%d", &age);
+    printf("Hai %d anni\n", age);
+    return 0;
+}
